efe25d118b1cd1c8c91f1b49eec37765ffd2c6ed
Marvin Reyes <reyemarv@gmail.com>
2020-03-02 06:12:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,10 +13,11 @@ class Song
     @@all 
   end
   
-  def self.new_by_filename(name)
-    new_song = self.new(name) 
-    new_song.name = name.split(" - ")[1]
-    new_song
+  def self.new_by_filename(file)
+    new_song = file.split(" - ")[1] 
+    new_artist = file.split(" - ")[0]
+    song = self.new(new_song)
+    song.artist_name
   end 
     
 end 
