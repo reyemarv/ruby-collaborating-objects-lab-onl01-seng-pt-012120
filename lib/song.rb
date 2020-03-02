523babf99341821d48da92cbfb5ad6eb7fc15820
Marvin Reyes <reyemarv@gmail.com>
2020-03-02 06:23:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,15 @@ class Song
     @@all 
   end
   
-  def self.new_by_filename(file)
-    new_song = file.split(" - ") 
-    song = self.new(new_song[1])
-    song.artist = new_song[0]
+def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    #this is calling the #artist_name=
+    #it is a writer method that assigns that variable to artist
+    #you are not just assigning the string to artist, because you want the artist attribute to be an artist instance instead
+    song.artist_name = artist
     song
-  end 
+  end
     
 end 
